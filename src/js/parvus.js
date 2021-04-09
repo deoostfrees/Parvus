00611b3e4106549caa059adae666497472c323fe
de Oostfreese <68224180+deoostfreese@users.noreply.github.com>
2021-04-09 08:59:16 +0200

    Fix mislabeling for the previous button

--- src/js/parvus.js
+++ src/js/parvus.js
@@ -67,7 +67,7 @@ export default function Parvus (userOptions) {
         en: {
           lightboxLabel: 'This is a dialog window which overlays the main content of the page. The modal shows the enlarged image. Pressing the Escape key will close the modal and bring you back to where you were on the page.',
           lightboxLoadingIndicatorLabel: 'Image loading',
-          prevButtonLabel: 'Previous image',
+          previousButtonLabel: 'Previous image',
           nextButtonLabel: 'Next image',
           closeButtonLabel: 'Close dialog window'
         }
