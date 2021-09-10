cb78a31229a1b30734ecd536f8f05803ad124460
deoostfreese <68224180+deoostfreese@users.noreply.github.com>
2021-09-10 10:38:27 +0200

    Build CSS/ JS

--- dist/js/parvus.js
+++ dist/js/parvus.js
@@ -7,6 +7,7 @@
  *
  * MIT license
  */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
