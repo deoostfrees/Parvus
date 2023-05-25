223e367e8efbfce02cc92afd59d5c667cbd61cd4
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2023-05-25 16:41:37 +0200

    [TASK] Clean up

--- dist/js/parvus.js
+++ dist/js/parvus.js
@@ -1095,7 +1095,6 @@
         // Vertical swipe
         if (!isReducedMotion) {
           if (MOVEMENT_Y_DISTANCE <= 100) {
-            // Set to 96 because otherwise event listener 'transitionend' does not fire if is vertical dragging
             lightboxOverlay.style.opacity = lightboxOverlayOpacity - MOVEMENT_Y_DISTANCE / 100;
           }
         }
