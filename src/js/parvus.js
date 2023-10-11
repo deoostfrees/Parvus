bfd7ec98a0418cd864ec6be3c24d7fecd5db39ef
Gerd <2538708+drhino@users.noreply.github.com>
2023-10-11 04:40:42 +0200

    `l10n`: Fall back to the default language
    1. Allows to pass an empty object, undefined or null, which will use the default language.
    2. Populates any missing keys with the default language.
    
    Adds support for:
    - new Parvus({ l10n: undefined })
    - new Parvus({ l10n: null })
    - new Parvus({ l10n: {} })
    
    Currently:
    - `undefined`: fails
    - `null`: fails
    - `{}`: Shows `undefined` for any of the translation tags
    
    This allows for an easier use of the library.

--- src/js/parvus.js
+++ src/js/parvus.js
@@ -77,7 +77,11 @@ export default function Parvus (userOptions) {
 
     return {
       ...DEFAULT_OPTIONS,
-      ...userOptions
+      ...userOptions,
+      l10n: {
+        ...DEFAULT_OPTIONS.l10n,
+        ...userOptions.l10n
+      }
     }
   }
 
