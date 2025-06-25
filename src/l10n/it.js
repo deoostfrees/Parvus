c661f8268d9f84af1698b768cb2d855aa5f4de37
Antonio Trifirò <antonio@blackstudio.it>
2025-06-25 11:33:46 +0200

    Add Italian translations

---
+++ src/l10n/it.js
@@ -0,0 +1,11 @@
+export default {
+  lightboxLabel: 'This is a dialog window that overlays the main content of the page. The modal displays the enlarged image. Pressing the Escape key will close the modal and bring you back to where you were on the page.',
+  lightboxLoadingIndicatorLabel: 'Image loading',
+  lightboxLoadingError: 'The requested image cannot be loaded.',
+  controlsLabel: 'Controls',
+  previousButtonLabel: 'Previous image',
+  nextButtonLabel: 'Next image',
+  closeButtonLabel: 'Close dialog window',
+  sliderLabel: 'Images',
+  slideLabel: 'Image'
+}
