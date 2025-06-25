4d04d8dcb9987fa48b80dc6aea0cb97677c1d47d
slolo2000 <slolo2000@users.noreply.github.com>
2025-06-25 10:22:10 +0200

    French translation

---
+++ src/l10n/fr.js
@@ -0,0 +1,11 @@
+export default {
+  lightboxLabel: 'Il s\'agit d\'une boîte de dialogue superposée au contenu principal de la page. La fenêtre modale affiche l\'image agrandie. Appuyez sur la touche Échap pour fermer la fenêtre modale et revenir à l\'endroit où vous étiez sur la page.',
+  lightboxLoadingIndicatorLabel: 'Chargement de l\'image',
+  lightboxLoadingError: 'L\'image demandée ne peut pas être chargée',
+  controlsLabel: 'Contrôles',
+  previousButtonLabel: 'Image précédente',
+  nextButtonLabel: 'Image suivante',
+  closeButtonLabel: 'Fermer la fenêtre de dialogue',
+  sliderLabel: 'Images',
+  slideLabel: 'Image'
+}
